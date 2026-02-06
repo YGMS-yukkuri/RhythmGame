b449ff7bb5ce1cf04f5d9986f862cab8941dac69
yukkuri <reityan.sakura0104@gmail.com>
2026-02-07 00:31:57 +0900

    append

--- JS/judge.js
+++ JS/judge.js
@@ -15,7 +15,7 @@ const overJudgementSecIndex = {
     "NOM": { Cperfect: 0.050, perfect: 0.166, great: 0.199, bad: 0.200 },
     "HRD": { Cperfect: 0.050, perfect: 0.099, great: 0.166, bad: 0.200 },
     "EXP": { Cperfect: 0.025, perfect: 0.050, great: 0.099, bad: 0.166 },
-    "MAS": { Cperfect: 0.025, perfect: 0.050, great: 0.066, bad: 0.166 },
+    "MAS": { Cperfect: 0.025, perfect: 0.050, great: 0.099, bad: 0.166 },
     "REMAS": { Cperfect: 0.025, perfect: 0.050, great: 0.099, bad: 0.166 },
     "END": { Cperfect: 0.016, perfect: 0.033, great: 0.066, bad: 0.133 }
 };
